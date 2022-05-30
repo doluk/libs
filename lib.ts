1e1717f9645a4db070bf4e8e3b3201c6b54fee53
doluk <lukas.dobler@uni-konstanz.de>
2022-05-30 15:23:33 +0200

    initial commit

--- lib.ts
+++ lib.ts
@@ -5,10 +5,34 @@ export interface Lib {
 	name: string
 	url: string
 	language: string
-	loginWithToken: version
-	loginWithEmail: version
-	dynamicIP: LinkableString
-	troopInformation: LinkableString
-	events: LinkableString
+	loginWithToken: string
+	loginWithEmail: string
+	dynamicIP: string
+	troopInformation: string
+	events: string
+	clans_CurrentWar_Leaguegroup: string
+	clans_clannwarleagues_wars: string
+	clans_warlog: string
+	clan_search: string
+	clans_current_war: string
+	clans: string
+	clans_members: string
+	players: string
+	players_verify: string
+	leagues: string
+	leagues_season_ranking: string
+	league_info: string
+	leagues_seasons: string
+	warleagues: string
+	warleague_info: string
+	locations_rankings_clans: string
+	locations_rankings_players: string
+	locations_rankings_clan_versus: string
+	locations_rankings_player_versus: string
+	locations: string
+	location_info: string
+	goldpass: string
+	labels_player: string
+	labels_clan: string
 	note: string
 }
